ff44012e4a08b5f6f2ab50d0cbfd164780d360cf
falkTX <falktx@falktx.com>
2019-07-15 11:57:42 +0200

    Fix windows cross-compile script
    Signed-off-by: falkTX <falktx@falktx.com>

--- utils/pack-windows-cross.sh
+++ utils/pack-windows-cross.sh
@@ -26,7 +26,7 @@ rm -rf "$NAME-win32bit"
 rm -rf "$NAME-win64bit"
 
 _mingw32-build make clean
-_mingw32-build make HAVE_JACK=false HAVE_LIBLO=false HAVE_PROJM=false -j8
+_mingw32-build make HAVE_JACK=false HAVE_LIBLO=false HAVE_PROJM=false HAVE_CAIRO=false WINDOWS=true -j8
 for i in `ls bin/*-vst.dll`; do mv $i `echo $i | awk 'sub("-vst","")'`; done
 rm -rf bin/*ladspa* bin/*dssi* bin/*vst*
 mkdir -p "$NAME-win32bit"
@@ -35,7 +35,7 @@ cp utils/README-Windows.txt "$NAME-win32bit/README.txt"
 compressFolderAsZip "$NAME-win32bit"
 
 _mingw64-build make clean
-_mingw64-build make HAVE_JACK=false HAVE_LIBLO=false HAVE_PROJM=false -j8
+_mingw64-build make HAVE_JACK=false HAVE_LIBLO=false HAVE_PROJM=false HAVE_CAIRO=false WINDOWS=true -j8
 for i in `ls bin/*-vst.dll`; do mv $i `echo $i | awk 'sub("-vst","")'`; done
 rm -rf bin/*ladspa* bin/*dssi* bin/*vst*
 mkdir -p "$NAME-win64bit"
